62922f2ac493c2e23da1836d05afbd30310ba031
AbdreeyJulius <neil.kelly1024@gmail.com>
2020-05-03 00:44:20 +0300

    start of 4 module

---
+++ www/js/discAndBonPage.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+	$(".discount__Card").flip({
+		trigger: 'click',
+		speed: 1000
+	});
+
+	$(".discount__Card").on('flip:done',function() {
+		let isFlip = $(this).data("flip-model");
+		if(isFlip.isFlipped) {
+			$(this).parent().find("#using").removeClass("d_none");
+			$(this).parent().find("#addition").addClass("d_none");
+			$(this).find(".front img").css("overflow", "hidden");
+			$(this).find(".discount__text span").slideDown(1500);
+			$(this).animate({
+				marginBottom: '0px'
+			}, 500);
+		} else {
+			$(this).parent().find("#addition").removeClass("d_none");
+			$(this).parent().find("#using").addClass("d_none");
+			$(this).find(".front img").css("overflow", "initial");
+			$(this).find(".discount__text span").slideUp(1500);
+			$(this).animate({
+				marginBottom: '68px'
+			}, 500);
+		}
+	});
+
+	$(".discount input[value='Подробнее']").on("click", function() {
+		$(this).parent().find(".discount__Card").flip(true);
+	});
+
+});
